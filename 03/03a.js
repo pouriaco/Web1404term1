c8bc97569d32cf6c8771c81e4d2046fce7b9fcea
pouriaco <p.pouriaco@gmail.com>
2025-10-28 14:57:09 +0330

    session3 03a

---
+++ 03/03a.js
@@ -0,0 +1,103 @@
+// let myInfo = {
+//     name : "pouria",
+//     family : "vahedi",
+//     age : 18
+// };
+let fs = require('fs');
+
+let inputs = process.argv.slice(3);
+let command = process.argv[2];
+
+switch (command) {
+    case "sum":
+        console.log(Number(nums[0]) + Number(nums[1]));
+        break;
+
+    case "minus":
+        console.log(Number(nums[0]) - Number(nums[1]));
+        break;
+
+    case "info":
+        let myInfo = {
+            name: inputs[0],
+            family: inputs[1],
+            age: inputs[2]
+        }
+
+        console.log("my information is : ", myInfo);
+        break;
+
+    case "print":
+        let obj = {
+            name: inputs[0],
+            family: inputs[1],
+            age: inputs[2]
+        }
+
+        // console.log("my information is : " , myInfo);
+        for (const key in obj) {
+            console.log("Salam ", obj[key]);
+        }
+        break;
+
+    case "write":
+        let outputJson = Write("salam");
+        let outputString = JSON.stringify(outputJson);
+        fs.writeFile("./data.txt", outputString, (err) => {
+            console.log(2);
+            if (err) {
+                console.log("file did not save!");
+            }
+            else {
+                console.log("file saved!");
+            }
+        });
+        console.log(3);
+
+        break;
+    case "read":
+
+        fs.readFile("./data.txt", { encoding: "utf8" }, (err, body) => {
+            if (err) {
+                console.log("file did not save!");
+            }
+            else {
+                console.log("file opened: ", body);
+            }
+        });
+
+        break;
+
+    case "append":
+        let text = process.argv[3];
+        fs.appendFile("./data.txt" , text , (err)=>{
+            console.log(2);
+            if (err) {
+                console.log("file did not save!");
+            }
+            else{
+                console.log("file saved!");
+            }
+        });
+        break;
+
+    default:
+        console.log("what are you talking about?")
+        break;
+}
+
+function Write(text) {
+    let obj2 = {
+        name: inputs[0],
+        family: inputs[1],
+        age: inputs[2]
+    }
+
+    let newObj = {}
+
+    for (const key in obj2) {
+        newObj[key] = text + " " + obj2[key];
+    }
+
+    return newObj;
+}
